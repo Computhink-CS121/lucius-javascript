3fc5c10e649f8455e48a488d12dda37d5bddca70
Computhink-CS121 <cs121@computhink.com.sg>
2025-07-16 08:12:33 +0000

    Jul 16, 2025, 8:12 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -4,7 +4,7 @@ Task 1: Add the setup() function
 
 function draw() {
   createCanvas(600, 400);
-  background(240); 
+  background(240); //
   // Challenge 1: An eye using ellipse() function
 
   // Challenge 2: A face using circle() function
