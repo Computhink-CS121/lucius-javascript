3ba17eb35adc9273c30aff2e3189fe6aa9dbb68f
Computhink-CS121 <cs121@computhink.com.sg>
2025-07-28 09:56:47 +0000

    Jul 28, 2025, 9:56 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -1,25 +1,26 @@
 function setup() {
     createCanvas(600, 400);
     background(220);
-    noLoop(); // Stops continuous drawing
+   
   }
   
 function draw() {
-  // Recap 1: Repeating Circles
-let posx =50;
-let posY =150;
-let circleD =100;
-let colourB =0
-for (let i=0; i<5; i++)
-fill(colourB);
-  posx = posx +
-  colourB 
-  circle( posx,posY,circleD);
-  circle(140,150,80);
-  circle(230,150,80);
-  circle(320,150,80);
-
-  }
+//   // Recap 1: Repeating Circles
+// let posx =50;
+// let posY =150;
+// let circleD =100;
+// let colourB =0
+// for (let i=0; i<5; i++)
+// fill(colourB,0,225);
+// function draw() {
+//   posx = posx +
+//   colourBfunction name(params) {
+//      = 
+//   }colourB + 30;
+//   circle( posx,posY,circleD);
+  
+//   posx = posx + circleD +10;
+  
   // Task 1: Colour Gradient
 
   // Task 2: Colour Loop
@@ -27,3 +28,315 @@ fill(colourB);
   // Task 3: Row of Circles
 
   // Task 4: Grid of Circles
+
+
+
+let xpos = 50;
+let ypos = 50;
+
+for (let i=0; i<5; i++)  {
+  if (i%2==0)
+fill("black");
+  else
+    fill("white")
+  rect(xpos, ypos, 50, 50);
+xpos = xpos + 60;
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
