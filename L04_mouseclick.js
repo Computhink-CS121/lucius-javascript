4276c1734ae0a24b97579cbecacfc40230f9a9de
Computhink-CS121 <cs121@computhink.com.sg>
2025-07-28 10:39:02 +0000

    Jul 28, 2025, 10:39 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -10,21 +10,20 @@ background(220);
 
 }
 function draw() {
-    let r = 0;
+//     let r = 0;
 
-    let g = 0;
-    let b = 0;
-    let xpos = 50;
-    let ypos = 50;
- // put your code inside here
-for (let i = 0; i < 5; i++) {
-fill(r,g,b);
-g=g + 50
-circle(xpos, ypos, 50);
-xpos = xpos + 50;
-ypos = ypos + 50;
-
+//     let g = 0;
+//     let b = 0;
+//     let xpos = 50;
+//     let ypos = 50;
+//  // put your code inside here
+// for (let i = 0; i < 5; i++) {
+// fill(r,g,b);
+// g=g + 50
+// circle(xpos, ypos, 50);
+// xpos = xpos + 50;
+// ypos = ypos + 50;
+noStroke()
 circle(mouseX, mouseY, 50);
 
-}
 }
