6227597c6e9dc111acb6a0b8c5bcb46b10d76f39
Computhink-CS121 <cs121@computhink.com.sg>
2025-08-15 10:49:48 +0000

    Aug 15, 2025, 10:49 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -1,72 +1,72 @@
 // write your codes here
 
-let soundpop;
-let picoImg, bgmusic;
+// let soundpop;
+// let picoImg, bgmusic;
 
-// preload()
-function preload(){
-    picoImg = loadImage('assets/pico-a.png');
-bgmusic = loadSound('assets/bossaNova.mp3');}
-soundpop = loadsound('assets/pop.mp3');
+// // preload()
+// function preload(){
+//     picoImg = loadImage('assets/pico-a.png');
+// bgmusic = loadSound('assets/bossaNova.mp3');}
+// soundpop = loadsound('assets/pop.mp3');
 
-// setup()
-function setup(){
-    createCanvas(600,400);
-    background("lightblue");
-    if (keyIsDown(RIGHT_ARROW)){
-        posx = posx + 5;
-    }
-    if (keyIsDown(LEFT_ARROW)){
-            posx = posx - 5;
-    }
+// // setup()
+// function setup(){
+//     createCanvas(600,400);
+//     background("lightblue");
+//     if (keyIsDown(RIGHT_ARROW)){
+//         posx = posx + 5;
+//     }
+//     if (keyIsDown(LEFT_ARROW)){
+//             posx = posx - 5;
+//     }
 
-    if (keyIsDown(UP_ARROW)){
-        posy = posy - 5;
-    }
-    if (keyIsDown(DOWN_ARROW)){
-        posy = posy + 5;
+//     if (keyIsDown(UP_ARROW)){
+//         posy = posy - 5;
+//     }
+//     if (keyIsDown(DOWN_ARROW)){
+//         posy = posy + 5;
 
-    }
+//     }
 
    
 
-bgmusic.loop();
-}
-// draw()
-function draw(){
+// bgmusic.loop();
+// }
+// // draw()
+// function draw(){
 
-    image(picoImg, 300, 200, 110, 133);
-    if (keyIsDown(RIGHT_ARROW)){
-        posx = posx + 5;
-    }
-    if (keyIsDown(LEFT_ARROW)){
-            posx = posx - 5;
-    }
+//     image(picoImg, 300, 200, 110, 133);
+//     if (keyIsDown(RIGHT_ARROW)){
+//         posx = posx + 5;
+//     }
+//     if (keyIsDown(LEFT_ARROW)){
+//             posx = posx - 5;
+//     }
 
-    if (keyIsDown(UP_ARROW)){
-        posy = posy - 5;
-    }
-    if (keyIsDown(DOWN_ARROW)){
-        posy = posy + 5;
+//     if (keyIsDown(UP_ARROW)){
+//         posy = posy - 5;
+//     }
+//     if (keyIsDown(DOWN_ARROW)){
+//         posy = posy + 5;
 
-    }
+//     }
 
-    posx = constrain(posx, 0, width - 30);
-    posy = constrain(posy, 0, width - 30);
+//     posx = constrain(posx, 0, width - 30);
+//     posy = constrain(posy, 0, width - 30);
 
-}
-
-
-
-bgmusic.loop()
+// }
 
 
-function keyPressed(){
-    soundpop.play();
-}
-function mousePressed(){
-    soundpop.play();
-}
+
+// bgmusic.loop()
+
+
+// function keyPressed(){
+//     soundpop.play();
+// }
+// function mousePressed(){
+//     soundpop.play();
+// }
 
 
 
