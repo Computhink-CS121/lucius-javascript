2dc4a5dd169e1dff939af25147c7f5251baa54a3
Computhink-CS121 <cs121@computhink.com.sg>
2025-08-15 10:46:28 +0000

    Aug 15, 2025, 10:46 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -13,6 +13,24 @@ soundpop = loadsound('assets/pop.mp3');
 function setup(){
     createCanvas(600,400);
     background("lightblue");
+    if (keyIsDown(RIGHT_ARROW)){
+        posx = posx + 5;
+    }
+    if (keyIsDown(LEFT_ARROW)){
+            posx = posx - 5;
+    }
+
+    if (keyIsDown(UP_ARROW)){
+        posy = posy - 5;
+    }
+    if (keyIsDown(DOWN_ARROW)){
+        posy = posy + 5;
+
+    }
+
+    posx = constrain(posx, 0, width - 30);
+    posy = constrain(posy, 0, width - 30);
+
 bgmusic.loop();
 }
 // draw()
