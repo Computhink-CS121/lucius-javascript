2085837c6a3c860e5b0e4493d352f0135d11e06c
Computhink-CS121 <cs121@computhink.com.sg>
2025-08-15 10:27:43 +0000

    Aug 15, 2025, 10:27 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -6,7 +6,7 @@ let picoImg, bgmusic;
 // preload()
 function preload(){
     picoImg = loadImage('assets/pico-a.png');
-bgmusic = loadsound('assets/pico-a.png');}
+bgmusic = loadsound('assets/.mp3');}
 
 
 // setup()
