42e05f487940105654457251a8bf4e85713d938b
Computhink-CS121 <cs121@computhink.com.sg>
2025-08-18 10:10:15 +0000

    Aug 18, 2025, 10:10 AM

--- testrevision.js
+++ testrevision.js
@@ -19,10 +19,10 @@ function draw() {
     // triangle(350, 200, 200, 300, 350, 350)
     for (let count=0; count<4; count++)  {
      fill('gray');
-        Rect(x, y, w, h)
+        rect(x, y, w, h)
         x =x+40;
         let gray = gray+ 50;
-        Rect(x, y, w, h)
+        rect(x, y, w, h)
     }
     
 }
