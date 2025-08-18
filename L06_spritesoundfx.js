a0b419fb604f80bf011ff6d229ffb14cccc71cfe
Computhink-CS121 <cs121@computhink.com.sg>
2025-08-18 10:25:17 +0000

    Aug 18, 2025, 10:25 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -82,7 +82,7 @@ let posx, posy;
 function preload(){
     picoImg = loadImage('assets/pico-a.png')
     soundeffect = loadsound('assets/pop.mp3')
-    bgMusic = loadsound('assets/')
+    bgMusic = loadsound('assets/bossaNova.mp3')
 }
 function setup(){
     createCanvas(400, 400);
