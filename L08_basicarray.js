1af34f60564060dcc86f571ab5f499d318547217
Computhink-CS121 <cs121@computhink.com.sg>
2025-09-08 10:26:48 +0000

    Sep 8, 2025, 10:26 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -3,9 +3,24 @@
 
 
 function preload() {
-let soundsfx = loadSound('')
+let soundsfx = loadSound('assets/star_wars_theme_8_bit.mp3');
+
 
 }
+function setup(){
+    createCanvas(600,400); // width, height
+background(80);
+}
+function draw(){
+    if (keyIsDown(32) )
+        soundsfx.play();
+
+}
+if (keyIsDown(Down_ARROW)) {
+  soundsfx.stop();
+
+}
+
 
 
 
