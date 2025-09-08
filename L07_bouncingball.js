734e897cdbf79ab49adcff3f728ef9f9df3f8fe6
Computhink-CS121 <cs121@computhink.com.sg>
2025-09-08 09:44:05 +0000

    Sep 8, 2025, 9:44 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -16,35 +16,35 @@ y = y + speedY;
 }
 
 
-function draw() {
-    background(80);
-    circle(x, height/2, 50);
+// function draw() {
+//     background(80);
+//     circle(x, height/2, 50);
 
-if (keyIsDown(LEFT_ARROW)) {
-x = x -5
-}
+// if (keyIsDown(LEFT_ARROW)) {
+// x = x -5
+// }
 
-if (x < 25) {
-    X = 25;
-}
-if (keyIsDown(RIGHT_ARROW)) {
+// if (x < 25) {
+//     X = 25;
+// }
+// if (keyIsDown(RIGHT_ARROW)) {
 
-    x = x +5
-}
+//     x = x +5
+// }
 
-if (x > 400-25) {
-    X = 400-25;
-}
-}
+// if (x > 400-25) {
+//     X = 400-25;
+// }
+// }
 
-if (keyIsDown(UP_ARROW)) {
+// if (keyIsDown(UP_ARROW)) {
 
-    x = x +5
-}
+//     x = x +5
+// }
 
-if (x > 400-25) {
-    X = 400-25;
-}
+// if (x > 400-25) {
+//     X = 400-25;
+// }
 
 
 
