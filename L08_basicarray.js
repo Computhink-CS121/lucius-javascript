0d454f74ba49bb599a9525e73321d46a0058ef2d
Computhink-CS121 <cs121@computhink.com.sg>
2025-09-08 10:36:53 +0000

    Sep 8, 2025, 10:36 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -10,6 +10,7 @@ soundsfx = loadSound('assets/star_wars_theme_8_bit.mp3');
 function setup(){
     createCanvas(600,400); // width, height
 background(80);
+fill('cyan')
 textSize(32);
 text("Hello my friend!", 20, 50);
 }
