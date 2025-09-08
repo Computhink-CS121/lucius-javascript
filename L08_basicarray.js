e3573408aeb5b3767e1b48559aba31909dba19cd
Computhink-CS121 <cs121@computhink.com.sg>
2025-09-08 10:45:55 +0000

    Sep 8, 2025, 10:45 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -16,7 +16,7 @@ text("Hello my friend!", 20, 50);
 text("Today is Monday!", 20, 90);
 text("My name is 67!", 20, 50);
 text("I am 69 this year!", 20, 90);
-text("my favourite !", 20, 90);
+text("my favourite activity is grieving in games!", 20, 90);
 
 let xpos = 100;
 for (let count=0; count<5; count++) {
