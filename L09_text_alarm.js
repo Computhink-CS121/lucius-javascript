d743b3322bf0ef6166e486f9ff008fbfa21342e3
Computhink-CS121 <cs121@computhink.com.sg>
2025-09-22 10:24:18 +0000

    Sep 22, 2025, 10:24 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -17,7 +17,7 @@ function setup(){
       background("pink");
 
      let aHour = hour() -12;
-     aHour
+     aHour = nf(aHour)
      let aMinute = minute();
      let aSecond = second();
       textSize(32);
