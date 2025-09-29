db799dfaa66b65a1989446acd46c7521dd519416
Computhink-CS121 <cs121@computhink.com.sg>
2025-09-29 09:38:17 +0000

    Sep 29, 2025, 9:38 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -34,6 +34,7 @@ function setup(){
       text("Countdown Timer", width/2, 200)
       text("0", width/2, 225)
 text("click me to start timer", width/2, 290);
+pie = 3.141
  function mousePressed(){
      sfx.play();
  }
