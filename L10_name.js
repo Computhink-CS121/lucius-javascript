becdeb8886b637002a677108c7798189a245f609
Computhink-CS121 <cs121@computhink.com.sg>
2025-09-29 11:00:29 +0000

    Sep 29, 2025, 11:00 AM

--- L10_name.js
+++ L10_name.js
@@ -7,7 +7,7 @@ function setup(){
      background("bgcolor");
 
      colorPicker = createColorPicker();
-     colorPicker.position(width/2, height/-102)
+     colorPicker.position(width/2, height/10)
 }
 
 
