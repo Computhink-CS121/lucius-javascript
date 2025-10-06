f3172cb77af8efb94860b5018c629fd0f8a3ae4f
Computhink-CS121 <cs121@computhink.com.sg>
2025-10-06 09:49:59 +0000

    Oct 6, 2025, 9:49 AM

--- L10_name.js
+++ L10_name.js
@@ -14,7 +14,7 @@ function setup(){
      colorPicker.position(width/2, height/2+85)
 }
 
- userInput = createInput
+ userInput = createInput(username);
 function draw() {
     background( colorPicker.value());
     fill("white");
