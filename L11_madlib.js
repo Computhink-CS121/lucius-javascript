03bf8f52135097dcdb37ac276ff99fe77a8eb352
Computhink-CS121 <cs121@computhink.com.sg>
2025-10-13 10:10:55 +0000

    Oct 13, 2025, 10:10 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -21,7 +21,7 @@ story = 0;
 sometextInput = createInput();
 sometextInput.position(50,50);
 }
-function changeText("e.g. a boy"){
+function changeText(){
    story = sometextInput.value();
     story++
 }
