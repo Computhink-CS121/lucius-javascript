7dcca0f04b3caa32ebc7fba159510521b6e23563
Computhink-CS121 <cs121@computhink.com.sg>
2025-10-13 09:43:34 +0000

    Oct 13, 2025, 9:43 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -2,11 +2,11 @@ let button1;
 function setup() {
 createCanvas(600,400); // width, height
 background("black");
-}
+
 
 button1 = createButton("Click on me!");
 button1.position(100,80)
- 
+}
 
 
 function draw() {
