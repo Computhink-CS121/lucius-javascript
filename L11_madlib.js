229403862c14588a73c112311b979aa88d1cfa51
Computhink-CS121 <cs121@computhink.com.sg>
2025-10-13 10:13:15 +0000

    Oct 13, 2025, 10:13 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -13,7 +13,7 @@ button1 = createButton("Click on me!");
 button1.position(50,50)
 button1.size(100,80)
 button1.mousePressed(changeText)
-story = 0;
+
 
 sometextInput = createInput("e.g. a boy");
 sometextInput.position(50,50);
