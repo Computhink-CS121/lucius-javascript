3d9ae3c5ebdb6936fd4676a7618092a6f6de546b
Computhink-CS121 <cs121@computhink.com.sg>
2025-11-10 10:13:44 +0000

    Nov 10, 2025, 10:13 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -36,8 +36,7 @@ function generateHints() {
 }
 
 function checkAnswer() {
-    let userinput = textInput.value().toUpperCase
-
+    let userinput = textInput.value().toUpperCase()
     if(userinput === selected){
     message = "You guessed it! The hidden word is 'beach";
     }
